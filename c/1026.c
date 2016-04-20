d9ff237eb3434851dd605285eef2293c2e30a497
ZGWJ <weijieguo897@gmail.com>
2016-04-20 16:44:05 +0800

    1026

--- c/1026.c
+++ c/1026.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    int hour,min,C1,C2;
-    float C3,sec;
+    int hour,min,sec,C1,C2,C3;
     scanf("%d %d",&C1,&C2);
-    C3=(C2-C1)/100.0;
+    C3=C2-C1;
+    if (C3%100>=50) {
+        C3+=50;
+    }
+    C3=C3/100;
     hour=C3/3600;
-    min=(int)C3%3600/60;
-    sec=C3-3600*hour-60*min;
-    if (sec-(int)sec>=0.5){
-        sec++;
-    }
-    if ((int)sec==60) {
-        sec=0;
-        min++;
-        if (min==60) {
-            min=0;
-            hour++;
-        }
-    }
-    printf("%02d:%02d:%02d\n",hour,min,(int)sec);
+    min=C3%3600/60;
+    sec=C3%60;
+    printf("%02d:%02d:%02d\n",hour,min,sec);
 }
