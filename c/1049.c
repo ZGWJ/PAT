98f9f2a030737e2d84ae714bd64f39453b5b28fb
ZGWJ <weijieguo897@gmail.com>
2016-04-23 18:54:48 +0800

    1049

--- c/1049.c
+++ c/1049.c
@@ -8,6 +8,8 @@ int main(){
     for (int i=0; i<N; i++) {
         scanf("%lf",&a);
         sum+=a*(double)(N-i)*(double)(i+1);
+        //比如，2.6在double下是2.599999999...，若乘int则误差放大导致错误！！故
+        //必须乘double!!!!!
     }
     
     printf("%.2lf",sum);
